3d721192e110525e2c28bba14b2cbb6300a7f5a1
Gitanjali Sahoo <gr.sahoo15@gmail.com>
2023-05-25 10:51:56 +0200

    image folder moved to asset and footer is not finished

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,34 +1,32 @@
 //import the headphone image
-import headphone from "../images/headphone.jpg";
+import headphone from '../assets/images/headphone.jpg'
 
-function Hero () {
+function Hero() {
     return (
         <div>
             <div className="hero-container">
                 <div className="hero-image">
- <img src={headphone} alt="" />
-</div>
+                    <img src={headphone} alt="" />
+                </div>
                 <div className="hero-text">
                     <div>
+                        <h3>
+                            {' '}
+                            <span>Empowering your world,</span> one <br />
+                            device at a time
+                        </h3>
+                        <p>
+                            Where Quality Meets Innovation. <br />
+                            Your Destination for High-Tech Wonders.
+                        </p>
+                    </div>
 
-                    <h3> <span>Empowering your world,</span> one <br />
-                        device at a time</h3>
-                    <p>Where Quality Meets Innovation. <br />
-Your Destination for High-Tech Wonders.</p>
-</div>
-
-                    <div >
-
+                    <div>
                         <button>Buy</button>
-  </div>
+                    </div>
                 </div>
-
-
             </div>
-
         </div>
-
-
     )
 }
-export default Hero;
+export default Hero
