1609bd9081f0b1bd451aa02ddb86610c66de35bd
sara <saraleepark1994@gmail.com>
2023-05-28 21:01:31 +0900

    sign in/sign up added , and hero is updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,16 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Hero from './components/Hero'
+import Footer from './components/Footer'
 function App() {
 
 
   return (
       <>
 
-          <Hero/>
+          <Hero />
+          <Footer/>
+
 
     </>
   )
