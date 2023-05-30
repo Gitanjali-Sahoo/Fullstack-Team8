024f1dfcad78bbbf3939a0b5463a6ee4cd5e304b
sara <saraleepark1994@gmail.com>
2023-05-30 16:33:45 +0900

    Navbar, hero, footer updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,24 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Footer from './components/Footer'
+import{ useState } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './components/Footer';
+import NavBar from './components/NavBar';
+import Hero from './components/Hero';
 
-
-import NavBar from './components/NavBar'
-import Hero  from './components/Hero'
 function App() {
+  const [showHero, setShowHero] = useState(true);
 
+  const handleIconClicked = () => {
+    setShowHero(false);
+  };
 
-    return (
-        <>
-
-            <NavBar />
-
-
-            <Hero />
-
-
-
-
-            <Footer />
-
-        </>
-
-    )
+  return (
+    <>
+      <NavBar onIconClicked={handleIconClicked} />
+      {showHero && <Hero  />}
+      <Footer />
+    </>
+  );
 }
 
 export default App;
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,4 +1,130 @@
-import { Facebook, Twitter, Apple } from 'react-bootstrap-icons'
+
+import { Facebook, Twitter, Apple, Google } from 'react-bootstrap-icons'
+import styled from 'styled-components'
+function Footer() {
+    return (
+        <Wrap>
+            <div className="footer-container">
+                <div className="footer-content">
+                    <div className="footer-text">
+                        <p>Contact Us</p>
+                        <p>About Us</p>
+                    </div>
+                    <div className="footer-copyright">
+                        <p>
+                            Copyright &copy; 2023 by Urban | All Rights
+                            Reserved.
+                        </p>
+                    </div>
+                    <div className="footer-icon">
+                        <Facebook />
+                        <Twitter />
+                        <Apple />
+                        <Google />
+                    </div>
+                </div>
+            </div>
+        </Wrap>
+    )
+}
+
+export default Footer
+
+// Using styled components
+const Wrap = styled.div`
+
+    .footer-container {
+        background-color: black;
+        color: white;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 20px;
+        padding: 20px;
+        margin-top: 20px;
+position:fixed;
+width: 100%
+top:0;
+bottom:0;
+left:0;
+right:0;
+
+    }
+
+    .footer-content {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+        mix-blend-mode: lighten;
+    }
+
+    .footer-text {
+        display: flex;
+        gap: 20px;
+    }
+
+    .footer-icon {
+        display: flex;
+        gap: 20px;
+    }
+
+
+
+    @media screen and (max-width: 768px) {
+        .footer-content {
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            position:fixed;
+
+
+
+margin-bottom:210px ;
+margin-top:150px;
+padding-bottom:100px;
+
+        }
+        .footer-text {
+   gap: 0px;
+            flex-direction: column;
+
+        }
+        .footer-copyright {
+            text-align: center;
+        }
+    }
+`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import { Facebook, Twitter, Apple } from 'react-bootstrap-icons'
 import styled from 'styled-components'
 
 function Footer() {
@@ -74,7 +200,7 @@ const Wrap = styled.div`
         .footer-container {
             flex-direction: row;
 
-            
+
 
         }
 
@@ -88,6 +214,6 @@ const Wrap = styled.div`
             flex-direction: row;
             gap: 20px;
         }
-
     }
 `
+*/
